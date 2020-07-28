f45fa00de066729f984b3b48e4703f864767df83
Jeremy Gale <jeremy.gale@atco.com>
2020-07-28 16:48:50 -0600

    Minimal bug where if #available(iOS 14, *) doesn't work properly on Xcode 11.6.

--- AvailableiOS14Bug/ContentView.swift
+++ AvailableiOS14Bug/ContentView.swift
@@ -7,9 +7,52 @@
 
 import SwiftUI
 
+struct NormalCaseHeader: ViewModifier {
+    @ViewBuilder
+    func body(content: Content) -> some View {
+        if #available(iOS 14, *) {
+            content.textCase(nil)
+        } else {
+            content
+        }
+    }
+
+    // This works on iOS 14 but doesn't compile in Xcode 11.6 for iOS 13. Error:
+    // Value of type 'NormalCaseHeader.Content' (aka '_ViewModifier_Content<NormalCaseHeader>') has no member 'textCase'
+//    func body(content: Content) -> some View {
+//        if #available(iOS 14, *) {
+//            return AnyView(content.textCase(nil))
+//        } else {
+//            return AnyView(content)
+//        }
+//    }
+}
+
+extension View {
+    func normalCase() -> some View {
+        return self.modifier(NormalCaseHeader())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Form {
+                Section(header: topContentView()) {
+                    Text("Item 1")
+                    Text("Item 2")
+                }
+                .normalCase()
+            }
+            .navigationBarTitle("#if available bug")
+        }
+    }
+    
+    private func topContentView() -> some View {
+        return VStack(alignment: .leading, spacing: 16) {
+            Text("lowercase text")
+                .font(.headline)
+        }
     }
 }
 
